63c3189d0e561e77c3daf1272adabe4324c06b34
maru <goo.mr.55.25@gmail.com>
2025-09-25 04:08:27 +0900

    example.py完成

---
+++ example.py
@@ -0,0 +1,13 @@
+print("hello world")
+
+def greet():
+    return print("こんにちは")
+
+def print_name(name):
+    return f"私の名前は{name}です"
+
+def get_greet():
+    return "おはようございます"
+
+def add(a, b):
+    return a + b
